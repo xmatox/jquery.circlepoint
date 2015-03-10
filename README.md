ed8fe69f274ae7132df8e7900a2c4e1e82042227
thomasabadie <xmatox@free.fr>
2015-03-10 15:19:13 +0100

    Add option : event change / mouse hover color

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# jquery.circlepoint v1.0.1
+# jquery.circlepoint v1.0.2
 jQuery Plugin for interactive point circles
 
 ### Usage
@@ -6,7 +6,7 @@ First, include the jQuery and circlepoint javascript files.
 
 ```html
 <script src="jquery.js" type="text/javascript"></script>
-<script src="jquery.circlepoint.js" type="text/javascript"></script>
+<script src="jquery.circlepoint.min.js" type="text/javascript"></script>
 ```
 Next, call the classic function
 
@@ -19,16 +19,47 @@ or with options
 
 ```html
 $(function($) {
-    $(".circlepoint").circlepoint({
-        nbPoint: 16,               // points / nombre de points
-        nbSize: 50,                // width / largueur (px)
-        sizePointSup: 10,          // size of the main points / taille des points principaux (px)
-        sizePointInf: 5,           // size of secondary points / taille des points secondaires (px)
-        sizePoint: 3,              // size of the other points / taille des autres points (px)
-        typePoint: "rond",         // type of dots / type de points (rond / carre)
-        bgdColorSup: "#123456",    // color of the main points / couleur des points principaux
-        bgdColorInf: "#987645",    // color secondary points / couleur des points secondaires
-        bgdColor: "#741852"        // color of the other points / couleur des autres points
+    $(".circlepoint_options").circlepoint({
+        nbPoint: 8,                             // points / nombre de points
+        totalSize: 50,                          // width / largueur (px)
+        input: false,                           // input id : false for hide it / false pour le désactiver
+        input_value: 0,                         // input value
+        input_fontSize: 15,                     // input font size
+        sizePointSup: 10,                       // size of the main points / taille des points principaux (px)
+        sizePointInf: 5,                        // size of secondary points / taille des points secondaires (px)
+        sizePoint: 3,                           // size of the other points / taille des autres points (px)
+        typePoint: "rond",                      // type of dots / type de points (rond / carre)
+        bgdColorSup: "#123456",                 // color of the main points / couleur des points principaux
+        bgdColorInf: "#987645",                 // color secondary points / couleur des points secondaires
+        bgdColor: "#741852",                    // color of the other points / couleur des autres points
+        bgdColorHover: "#FFCC00",               // color of points on mouse hover / couleur des points au survol
+        changeAction: null                      // change event
     });
 });
 ```
+or with options and change event
+
+```html
+$(function($) {
+    $(".circlepoint_options_event").circlepoint({
+        nbPoint: 32,                             // points / nombre de points
+        totalSize: 200,                          // width / largueur (px)
+        input: "circlepoint_input",              // input id : false for hide it / false pour le désactiver
+        input_value: 0,                          // input value
+        input_fontSize: 20,                      // input font size
+        sizePointSup: 20,                        // size of the main points / taille des points principaux (px)
+        sizePointInf: 10,                        // size of secondary points / taille des points secondaires (px)
+        sizePoint: 5,                            // size of the other points / taille des autres points (px)
+        typePoint: "rond",                       // type of dots / type de points (rond / carre)
+        bgdColorSup: "#0884FF",                  // color of the main points / couleur des points principaux
+        bgdColorInf: "#EC151D",                  // color secondary points / couleur des points secondaires
+        bgdColor: "#219D19",                     // color of the other points / couleur des autres points
+        bgdColorHover: "#FFCC00",                // color of points on mouse hover / couleur des points au survol
+        changeAction: function () {              // change event
+            $(".circlepoint_options_event__text").html("You choose : "+$( this ).val()+" degrees");
+        }
+    });
+});
+```
+
+
--- jquery.circlepoint.js
+++ jquery.circlepoint.js
@@ -1,5 +1,5 @@
 /*
-* Circlepoint v0.1
+* Circlepoint v1.0.2
 * jQuery Plugin for interactive point circles
 * https://github.com/xmatox/jquery-circlepoint
 *
@@ -7,62 +7,117 @@
 * Released under the MIT license
 */
 (function( $ ) {
+    // default settings:
+    var circlepoint_input = "circlepoint_input_"+Math.floor((Math.random() * 10000000) + 1)
+    var defaults = {
+        nbPoint: 16,                        // nombre de points
+        totalSize: 60,                      // largueur (px)
+        input: circlepoint_input,           // input id, false pour le désactiver
+        input_value: 0,                     // input id, false pour le désactiver
+        input_fontSize: 15,                 // input id, false pour le désactiver
+        sizePointSup: 10,                   // taille des points principaux (px)
+        sizePointInf: 6,                    // taille des points secondaux (px)
+        sizePoint: 4,                       // taille des autres points (px)
+        typePoint: "rond",                  // type de points (rond / carre)
+        bgdColorSup: "#333",                // couleur des points principaux
+        bgdColorInf: "#666",                // couleur des points secondaux
+        bgdColor: "#999",                   // couleur des autres points
+        bgdColorHover: "#FFCC00",           // couleur des points au survol
+        changeAction: null                  // change event
+    };
     jQuery.fn.circlepoint = function( options ) {
-        // default settings:
-        var defaults = {
-            nbPoint: 16,                // nombre de points
-            nbSize: 100,                // largueur (px)
-            sizePointSup: 20,           // taille des points principaux (px)
-            sizePointInf: 10,           // taille des points secondaux (px)
-            sizePoint: 5,               // taille des autres points (px)
-            typePoint: "rond",          // type de points (rond / carre)
-            bgdColorSup: "#333",        // couleur des points principaux
-            bgdColorInf: "#666",        // couleur des points secondaux
-            bgdColor: "#999"            // couleur des autres points
-        };
-        var o = $.extend( {}, defaults, options );
+        options = $.extend({}, defaults, options);
         return this.each(function() {
+
             var elem = $( this );
             elem.css("position","relative");
-            var size = o.nbSize/2;
-            var nbPoint = o.nbPoint;
+            var totalSize = options.totalSize;
+            elem.css("height",totalSize);
+            elem.css("width",totalSize);
+            var size = (totalSize-options.sizePointSup)/2;
+            if(options.input){
+                var new_input = "<input id='"+options.input+"' value='"+options.input_value+"'>";
+                var this_input = $(new_input).appendTo(elem);
+                var input_size = (totalSize-(options.sizePointSup*2));
+                var input_fontSize = options.input_fontSize;
+                var input_height = input_fontSize+2;
+                var input_posX = options.sizePointSup;
+                var input_posY = (totalSize/2)-(input_height/2);
+                this_input.css("position","absolute");
+                this_input.css("border",0);
+                this_input.css("margin",0);
+                this_input.css("padding",0);
+                this_input.css("text-align","center");
+                this_input.css("font-weight","bold");
+                this_input.css("vertical-align","middle");
+                this_input.css("background","none");
+                this_input.css("font-size",input_fontSize+"px");
+                this_input.css("width", input_size+"px");
+                this_input.css("height", input_height+"px");
+                this_input.css("left", input_posX+"px");
+                this_input.css("top", input_posY+"px");
+                this_input.on( "changeAction", options.changeAction);
+                this_input.change(function(){
+                    this_input.trigger("changeAction");
+                });
+            }
+            var nbPoint = options.nbPoint;
             var stepPoint = nbPoint/4;
             var stepPoint2 = nbPoint/8;
             points=getCirclePoints(size,size,size,nbPoint);
             $.each(points,function(i,p){
-                var sup_class = "";
-                var this_size = o.sizePoint;
+                var bgdColor = options.bgdColor;
+                var bgdColorHover = options.bgdColorHover;
+                var sizePoint = options.sizePoint;
+                var this_size = options.sizePoint;
                 if((i%stepPoint) == 0){
-                    sup_class = 'jcp__point__sup';
-                    this_size = o.sizePointSup;
+                    bgdColor = options.bgdColorSup;
+                    sizePoint = options.sizePointSup;
+                    this_size = options.sizePointSup;
                 }else if((i%stepPoint2) == 0){
-                    sup_class = 'jcp__point__inf';
-                    this_size = o.sizePointInf;
+                    bgdColor = options.bgdColorInf;
+                    sizePoint = options.sizePointInf;
+                    this_size = options.sizePointInf;
                 }
-                var newPoint = "<div class='jcp__point "+sup_class+"'></div>";
-                var this_x = p.x-this_size/2;
-                var this_y = p.y-this_size/2;
-                $(newPoint).appendTo(elem).css('top',this_x+'px').css('left',this_y+'px');
+                var random_id = Math.floor((Math.random() * 10000000) + 1);
+                var newPoint = "<div id='jcp__point_"+random_id+"'></div>";
+                var this_x = options.sizePointSup/2+p.x-this_size/2;
+                var this_y = options.sizePointSup/2+p.y-this_size/2;
+                var this_angle = p.angle-180;
+                if(this_angle == -180) this_angle = 180;
+                this_angle = Math.round(this_angle*100)/100;
+                var this_point = $(newPoint).appendTo(elem);
+                this_point.css('top',this_x+'px').css('left',this_y+'px');
+                this_point.css("position","absolute");
+                if(options.typePoint == "rond") this_point.css("border-radius","50%");
+                this_point.css("background-color",bgdColor);
+                this_point.css("height", sizePoint+"px");
+                this_point.css("width", sizePoint+"px");
+                this_point.css("cursor", "pointer");
+                this_point.attr("data-angle", this_angle);
+                this_point.hover(function(){
+                    this_point.css("background-color",bgdColorHover);
+                }, function() {
+                    this_point.css("background-color",bgdColor);
+                });
+                this_point.click(function(){
+                    var click_angle = $(this).attr("data-angle");
+                    if(options.input){
+                        $("#"+options.input).val(click_angle);
+                        $("#"+options.input).trigger("changeAction");
+                    }
+                });
             });
-            $(".jcp__point").css("position","absolute");
-            if(o.typePoint == "rond") $(".jcp__point").css("border-radius","50%");
-            $(".jcp__point").css("background-color",o.bgdColor);
-            $(".jcp__point").css("height", o.sizePoint+"px");
-            $(".jcp__point").css("width", o.sizePoint+"px");
-            $(".jcp__point__inf").css("background-color",o.bgdColorInf);
-            $(".jcp__point__inf").css("height",o.sizePointInf);
-            $(".jcp__point__inf").css("width",o.sizePointInf);
-            $(".jcp__point__sup").css("background-color",o.bgdColorSup);
-            $(".jcp__point__sup").css("height",o.sizePointSup);
-            $(".jcp__point__sup").css("width",o.sizePointSup);
         });
     };
     function getCirclePoints(centerX,centerY,radius,segments){
         var totalPoints=new Array();
         for(var i=0;i<segments;i++){
-            x=centerX+radius*Math.sin(i*2*Math.PI/segments);
-            y=centerY+radius*Math.cos(i*2*Math.PI/segments);
-            totalPoints.push({'x':x,'y':y});
+            var angle_radian = i*2*Math.PI/segments;
+            var angle_degree = angle_radian * 180 / Math.PI;
+            x=centerX+radius*Math.sin(angle_radian);
+            y=centerY+radius*Math.cos(angle_radian);
+            totalPoints.push({'x':x,'y':y,'angle':angle_degree});
         }
         return totalPoints;
     };
